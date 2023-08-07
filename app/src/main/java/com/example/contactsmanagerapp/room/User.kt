f208559a70c6ef9ef650905497412b6d0a99de01
Abinash Acharya <075bei004.abinash@pcampus.edu.np>
2023-08-07 15:05:13 +0545

    General Layout added.

---
+++ app/src/main/java/com/example/contactsmanagerapp/room/User.kt
@@ -0,0 +1,20 @@
+package com.example.contactsmanagerapp.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+//If entity name not given room will use class name as the main name.
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_id")
+    //Without the specific name of the column it'll work using default name.
+    val id: Int,
+
+    @ColumnInfo(name = "user_name")
+    val name: String,
+
+    @ColumnInfo(name= "user_email")
+    val email: String
+)
